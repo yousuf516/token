a79a41e66f3babbc78aa61b24a83bc03a000cb4a
INTEL <yousufzubair1234@gmail.com>
2023-01-19 23:15:05 +0500

    Made it so new user can claim free 10000 tokens and check how much tokens they got by entering their principal id and also users can transfer tokens to other users by using their principal id

--- src/token_backend/main.mo
+++ src/token_backend/main.mo
@@ -1,10 +1,11 @@
 import Principal "mo:base/Principal";
 import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
 
 actor {
 
     var owner : Principal = Principal.fromText("fxmz7-ofvtd-vtacc-d6u6z-hhudm-ubfnf-4advo-wtky4-qymbo-rx4zp-iae");
-    var totalSupply : Nat = 1000000000;
+    var totalSupply : Nat = 100000000000000;
     var symbol = "DOU";
 
     var balances = HashMap.HashMap<Principal, Nat>(1, Principal.equal, Principal.hash);
@@ -25,5 +26,39 @@ actor {
     public query func getSymbol() : async Text {
         return symbol;
     };
+
+    public shared(msg) func payOut() : async Text {
+        if (balances.get(msg.caller) == null) {
+            let amount = 10000;
+            balances.put(msg.caller, amount);
+            return "Success";
+        } else {
+            return "Already Claimed";
+        }
+    };
+
+    public shared(msg) func transfer(to: Principal, amount: Nat) : async Text {
+        
+        // Taking users balance to check if he have enought amount to send
+
+        let fromBalance = await balanceOf(msg.caller);
+        if (fromBalance >= amount) {
+
+            // Subtracting balance from sender's account
+
+            let newFromBalance : Nat = fromBalance - amount;
+            balances.put(msg.caller, newFromBalance);
+
+            // Adding new balance to reciever's account
+
+            let toBalance = await balanceOf(to);
+            let newToBalance = toBalance + amount;
+            balances.put(to, newToBalance);
+
+            return "Success";
+        } else {
+            return "Insufficent Funds";
+        }
+    }
 
 }
--- src/token_frontend/src/components/Transfer.jsx
+++ src/token_frontend/src/components/Transfer.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Principal } from "@dfinity/principal";
+import { token_backend } from "../../../declarations/token_backend";
 
 function Transfer() {
   
+  const [isStuck, setStuck] = React.useState(true)
+  const [isDisabled, setDisabled] = React.useState(false);
+  const [recipientId, setId] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [buttonText, setText] = React.useState("Transfer");
+
   async function handleClick() {
+    setDisabled(true);
     
+    setTimeout(function () {
+      if(isStuck === true) {
+        setText("Check Account ID");
+        setDisabled(false);
+      };
+    }, 15000);
+
+    const recipient = Principal.fromText(recipientId);
+    const amountToTransfer = Number(amount);
+
+    let result = await token_backend.transfer(recipient, amountToTransfer);
+
+    setTimeout(function () {
+      setText("Transfer");
+    }, 14000);
+
+    if(result == "Success") {
+      setText("Successfully Sent");
+      setDisabled(false);
+      setStuck(false);
+    } else if (result == "Insufficent Funds") {
+      setText("Insufficent Funds");
+      setDisabled(false);
+      setStuck(false);
+    }
+
   }
 
   return (
@@ -16,6 +51,8 @@ function Transfer() {
               <input
                 type="text"
                 id="transfer-to-id"
+                value={recipientId}
+                onChange={(e) => setId(e.target.value)}
               />
             </li>
           </ul>
@@ -27,13 +64,15 @@ function Transfer() {
               <input
                 type="number"
                 id="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
               />
             </li>
           </ul>
         </fieldset>
         <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
-            Transfer
+          <button id="btn-transfer" onClick={handleClick} disabled={isDisabled}>
+            {buttonText}
           </button>
         </p>
       </div>
